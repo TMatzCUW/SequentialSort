08d4a8a4fcfe54e125227e808f914649939d5e1e
TMatzCUW <70924911+TMatzCUW@users.noreply.github.com>
2022-03-02 20:45:12 -0600

    Works, timeit is confusing with expressions and can't quite figure out how to make it work, but the entire code does finish almost instantly on my laptop

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import random
+import timeit
 
-def makeit(arr, lower, upper, amount):
-    arr.append(random.sample(range(lower, upper), amount))
+
+def makeit(arr, lower, upper):
+    for i in range(lower,upper):
+        arr.append(i)
+    random.shuffle(arr)
 
 stuffy=[]
-makeit(stuffy,1,50,10)
+makeit(stuffy,1,1001)
 print(stuffy)
 #stuff=[1,5,8,9,3,2]
 
@@ -16,7 +20,7 @@ def SelctionSort(arr):
                 small=j #if the position of the smallest number has a number larger than something else, it isn't the smallest
                 #this keeps looping until it has the smallest number left
         arr[i], arr[small]= arr[small], arr[i] #this swaps it
-    return arr
+    print(arr)
 
 def InsertionSort(arr):
     for i in range(1, len(arr)):
@@ -25,7 +29,7 @@ def InsertionSort(arr):
         while arr[small-1] > number and small>0:
             arr[small]#swap here
             number=number-1
-    return arr
+    print(arr)
 
 def BubbleSort(arr):
     n=len(arr)
@@ -33,8 +37,53 @@ def BubbleSort(arr):
         for j in range(0,n-i-1):
             if arr[j]>arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1],arr[j]
+    print(arr)
 
-sortedstuff=SelctionSort(stuffy)
-#sortedstuff=InsertionSort(stuff)
-sortedstuff=BubbleSort(stuffy)
-print(sortedstuff)
+
+# Python program for implementation of MergeSort
+def mergeSort(arr):
+    if len(arr) > 1:
+
+        # Finding the mid of the array
+        mid = len(arr) // 2
+
+        # Dividing the array elements
+        L = arr[:mid]
+
+        # into 2 halves
+        R = arr[mid:]
+
+        # Sorting the first half
+        mergeSort(L)
+
+        # Sorting the second half
+        mergeSort(R)
+
+        i = j = k = 0
+
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+    print(arr)
+
+SelctionSort(stuffy)
+InsertionSort(stuffy)
+BubbleSort(stuffy)
+mergeSort(stuffy)
